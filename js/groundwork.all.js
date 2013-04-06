e5b778163a76c27cab9e9085d4f34957a47ea78b
Gary Hepting <engineering+gary@sidereel.com>
2013-04-06 13:06:58 -0700

    added conditional modifier class for navigation components - closes #78

--- js/groundwork.all.js
+++ js/groundwork.all.js
@@ -549,34 +549,34 @@
     openMenu = function(target) {
       return $(target).parent('li.menu').toggleClass('on');
     };
-    $('body').on('mouseenter', 'nav > ul > li.menu:not(.disabled)', function(e) {
+    $('body').on('mouseenter', '.nav > ul > li.menu:not(.disabled)', function(e) {
       if ($(window).width() >= 768) {
         clearTimeout(delay);
-        $('nav > ul > li.menu.on').removeClass('on');
+        $('.nav > ul > li.menu.on').removeClass('on');
         return $(this).addClass('on');
       }
     });
-    $('body').on('mouseleave', 'nav > ul > li.menu:not(.disabled)', function(e) {
+    $('body').on('mouseleave', '.nav > ul > li.menu:not(.disabled)', function(e) {
       if ($(window).width() >= 768) {
         return delay = setTimeout((function() {
-          return $('nav > ul > li.menu.on').removeClass('on');
+          return $('.nav > ul > li.menu.on').removeClass('on');
         }), 350);
       }
     });
-    $('body').on('click', 'nav > ul > li.menu:not(.disabled) > a', function(e) {
+    $('body').on('click', '.nav > ul > li.menu:not(.disabled) > a', function(e) {
       if (Modernizr.touch || $(window).width() < 768) {
         openMenu(e.target);
       } else {
-        $('nav > ul > li.menu.on').removeClass('on');
+        $('.nav > ul > li.menu.on').removeClass('on');
         $(e.target).parents('li.menu').addClass('on');
       }
       e.preventDefault();
       return false;
     });
-    $('body').on('focus', 'nav > ul > li:not(.on) > a', function() {
-      return $('nav > ul > li.menu.on').removeClass('on');
+    $('body').on('focus', '.nav > ul > li:not(.on) > a', function() {
+      return $('.nav > ul > li.menu.on').removeClass('on');
     });
-    $('body').on('focus', 'nav > ul > li.menu > a', function(e) {
+    $('body').on('focus', '.nav > ul > li.menu > a', function(e) {
       openMenu(e.target);
       e.preventDefault();
       return false;
@@ -589,8 +589,8 @@
           $('.dropdown').filter('.on').removeClass('on');
         }
       }
-      if ($('nav > ul > li').filter('.menu.on').length) {
-        return $('nav > ul > li').filter('.menu.on').removeClass('on');
+      if ($('.nav > ul > li').filter('.menu.on').length) {
+        return $('.nav > ul > li').filter('.menu.on').removeClass('on');
       }
     });
     $('body').on('focus', '.dropdown', function(e) {
@@ -599,7 +599,7 @@
     $('body').on('blur', '.dropdown li:last-child a', function(e) {
       return $('.dropdown').filter('.on').removeClass('on');
     });
-    $('nav.menu').each(function() {
+    $('.nav.menu').each(function() {
       if (!$(this).attr('data-label')) {
         $(this).attr('data-label', 'Menu');
       }
@@ -607,22 +607,22 @@
         return $(this).prepend('<a href="#" class="menu-toggle"><i class="icon-reorder"></i></a>');
       }
     });
-    $('body').on('click', 'nav.menu .menu-toggle', function(e) {
-      $(this).parent('nav.menu').toggleClass('on');
+    $('body').on('click', '.nav.menu .menu-toggle', function(e) {
+      $(this).parent('.nav.menu').toggleClass('on');
       e.preventDefault();
       return false;
     });
     $('body').on('focus', '.menu-toggle', function(e) {
-      return $(e.target).parent('nav.menu').addClass('on');
+      return $(e.target).parent('.nav.menu').addClass('on');
     });
-    $('body').on('blur', 'nav.menu > ul > li:last-child a', function(e) {
-      return $('nav.menu').filter('.on').removeClass('on');
+    $('body').on('blur', '.nav.menu > ul > li:last-child a', function(e) {
+      return $('.nav.menu').filter('.on').removeClass('on');
     });
   });
 
   $(window).on('resize', function() {
-    if ($('nav > ul > li.menu.on').length > 1) {
-      return $('nav > ul > li.menu.on').removeClass('on').first().addClass('on');
+    if ($('.nav > ul > li.menu.on').length > 1) {
+      return $('.nav > ul > li.menu.on').removeClass('on').first().addClass('on');
     }
   });
 
--- src/coffee/components/menus.coffee
+++ src/coffee/components/menus.coffee
@@ -5,30 +5,30 @@ $ ->
   openMenu = (target) ->
     $(target).parent('li.menu').toggleClass('on')
   # hover on
-  $('body').on 'mouseenter', 'nav > ul > li.menu:not(.disabled)', (e) ->
+  $('body').on 'mouseenter', '.nav > ul > li.menu:not(.disabled)', (e) ->
     if $(window).width() >= 768
       clearTimeout(delay)
-      $('nav > ul > li.menu.on').removeClass('on')
+      $('.nav > ul > li.menu.on').removeClass('on')
       $(this).addClass('on')
   # hover off
-  $('body').on 'mouseleave', 'nav > ul > li.menu:not(.disabled)', (e) ->
+  $('body').on 'mouseleave', '.nav > ul > li.menu:not(.disabled)', (e) ->
     if $(window).width() >= 768
       delay = setTimeout (->
-        $('nav > ul > li.menu.on').removeClass('on')
+        $('.nav > ul > li.menu.on').removeClass('on')
       ), 350
   # click/touch
-  $('body').on 'click', 'nav > ul > li.menu:not(.disabled) > a', (e) =>
+  $('body').on 'click', '.nav > ul > li.menu:not(.disabled) > a', (e) =>
     if Modernizr.touch || $(window).width() < 768
       openMenu(e.target)
     else
-      $('nav > ul > li.menu.on').removeClass('on')
+      $('.nav > ul > li.menu.on').removeClass('on')
       $(e.target).parents('li.menu').addClass('on')
     e.preventDefault()
     return false
   # keyboard accessibility
-  $('body').on 'focus', 'nav > ul > li:not(.on) > a', ->
-    $('nav > ul > li.menu.on').removeClass('on')
-  $('body').on 'focus', 'nav > ul > li.menu > a', (e) =>
+  $('body').on 'focus', '.nav > ul > li:not(.on) > a', ->
+    $('.nav > ul > li.menu.on').removeClass('on')
+  $('body').on 'focus', '.nav > ul > li.menu > a', (e) =>
     openMenu(e.target)
     e.preventDefault()
     return false
@@ -41,8 +41,8 @@ $ ->
     else
       if $('.dropdown').filter('.on').length
         $('.dropdown').filter('.on').removeClass('on')
-    if $('nav > ul > li').filter('.menu.on').length
-      $('nav > ul > li').filter('.menu.on').removeClass('on')
+    if $('.nav > ul > li').filter('.menu.on').length
+      $('.nav > ul > li').filter('.menu.on').removeClass('on')
   # keyboard accessibility
   $('body').on 'focus', '.dropdown', (e) ->
     $(this).addClass('on')
@@ -51,22 +51,22 @@ $ ->
 
 
   # hamburger menus
-  $('nav.menu').each ->
+  $('.nav.menu').each ->
     $(this).attr('data-label','Menu') unless $(this).attr('data-label')
     $(this).prepend('<a href="#" class="menu-toggle"><i class="icon-reorder"></i></a>') unless $(this).find('.menu-toggle').length > 0
-  $('body').on 'click', 'nav.menu .menu-toggle', (e) ->
-    $(this).parent('nav.menu').toggleClass('on')
+  $('body').on 'click', '.nav.menu .menu-toggle', (e) ->
+    $(this).parent('.nav.menu').toggleClass('on')
     e.preventDefault()
     return false
   # keyboard accessibility
   $('body').on 'focus', '.menu-toggle', (e) ->
-    $(e.target).parent('nav.menu').addClass('on')
-  $('body').on 'blur', 'nav.menu > ul > li:last-child a', (e) ->
-    $('nav.menu').filter('.on').removeClass('on')
+    $(e.target).parent('.nav.menu').addClass('on')
+  $('body').on 'blur', '.nav.menu > ul > li:last-child a', (e) ->
+    $('.nav.menu').filter('.on').removeClass('on')
 
 
   return
 
 $(window).on 'resize', ->
-  if $('nav > ul > li.menu.on').length > 1
-    $('nav > ul > li.menu.on').removeClass('on').first().addClass('on')
+  if $('.nav > ul > li.menu.on').length > 1
+    $('.nav > ul > li.menu.on').removeClass('on').first().addClass('on')
